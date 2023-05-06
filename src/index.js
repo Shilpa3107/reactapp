81c819599ba86bf9b252bca16e5559efefa2880f
Shilpa <shilpa.sinha3107@gmail.com>
2023-05-07 03:28:23 +0530

    Finding current date and time

--- src/index.js
+++ src/index.js
@@ -2,17 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const fname = "Shilpa Sinha";
-const img1 = "https://e1.pxfuel.com/desktop-wallpaper/758/579/desktop-wallpaper-toedit-bts-jungkook-cute-soft-btsjeonjung-jungkook-cute-pics.jpg";
-const img2 = "https://e0.pxfuel.com/wallpapers/132/264/desktop-wallpaper-kaldur%E3%80%90%E2%98%AF%E3%80%91%E2%80%BF%E3%80%90%E2%98%AF%E3%80%91-cute-jungkook-pics-a-thread-cute-jung-kook-bts.jpg";
-const img3 = "https://e0.pxfuel.com/wallpapers/389/963/desktop-wallpaper-bts-cute-pics-bts-girls.jpg";
-const links = "https://www.pxfuel.com/en/query?q=bts+cute+pics";
+const date = new Date().toLocaleDateString();
+const time = new Date().toLocaleTimeString();
+
 ReactDOM.render(
 <>
-<h1 contentEditable="true">My name is {fname} </h1>,
-<a href = "https://www.pxfuel.com/en/query?q=bts+cute+pics" target = "_blankpage">
-<img src = {img1} alt = "jungkook" width = "200" height = "300"/>
-<img src = {img2} alt = "jungkook" width = "250" height = "300"/>
-<img src = {img3} alt = "bts" width = "250" height = "300"/>
-</a>
+<h1>{` Hello, My name is ${fname}`} </h1>
+<p>Today's Date is {date} </p>
+<p>Current Time is {time}</p>
 </>,
 document.getElementById('root')); 
