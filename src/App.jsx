e4a664adaa276dc5f474b3404e1a415e0987744b
Shilpa <shilpa.sinha3107@gmail.com>
2023-05-08 03:09:06 +0530

    importing values of calc

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import {add,sub,div,mult} from './Calc';
 
-
-function add(x,y){
-    return (x+y);
+function App(){
+    return (
+        <>
+     <ul>
+    <li>Sum of two no is {add(40,4)}</li>
+    <li>Sub of two no is {sub(30,3)}</li>
+    <li>Div of two no is {div(10,3)}</li>
+    <li>Mult of two no is {mult(10,3)}</li>
+     </ul>
+        
+        </>
+    );
 }
-function sub(x,y){
-    return (x-y);
-}
-function div(x,y){
-    return (x/y);
-}
-function mult(x,y){
-    return (x*y);
-}
- export {add,sub,div,mult};
+export default App;
