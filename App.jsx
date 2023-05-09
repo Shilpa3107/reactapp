cd2aca9939e8a734cdaff0440db2fb4d4ae105da
Shilpa3107 <116442351+Shilpa3107@users.noreply.github.com>
2023-05-09 18:15:11 +0530

    Netflix_web_series

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import Netflixseries from './Netflixseries';
+import Heading from './Heading';
+
+function App(){
+    return(
+        <>
+        <Heading/>
+        <Netflixseries/>
+        </>
+    );
+}
+export default App;
---
+++ Heading.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import "./index.css";
+
+function Heading(){
+   return (<h1 className = "heading">LIST OF 5 WEB SERIES OF NETFLIX</h1>);
+}
+
+export default Heading;
