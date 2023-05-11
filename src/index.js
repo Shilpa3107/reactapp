37de38f81b826847b8e9c8c5ee30c84c851cd3cd
Shilpa <shilpa.sinha3107@gmail.com>
2023-05-12 04:24:43 +0530

    Using useState function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App2 from './App2';
+import App3 from './App3';
 
 
-ReactDOM.render(<App2/>, document.getElementById('root'));
+ReactDOM.render(<App3/>, document.getElementById('root'));
