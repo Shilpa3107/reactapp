0fccc4ef26d07ad35efbb2e35d3d799a4b1242d9
Shilpa <shilpa.sinha3107@gmail.com>
2023-05-11 23:34:02 +0530

    Creating greeting message

---
+++ src/App2.jsx
@@ -0,0 +1,11 @@
+import Greeting from './Greeting';
+
+function App2(){
+    return (
+        <>
+        <Greeting/>
+
+        </>
+    );
+}
+export default App2;
---
+++ src/Greeting.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import "./index2.css";
+
+const time = new Date();
+const t = time.getHours();
+
+let k = " ";
+const cstyle = {
+    backgroundColor : "lightgoldenrodyellow"
+};
+ if(t>=1 && t<12){
+    k = "Good Morning";
+    cstyle.color = "green";
+ }
+ else if(t>=12 && t<7){
+    k = "Good Afternoon";
+    cstyle.color = "orange";
+ }
+ else{
+    k = "Good Night";
+    cstyle.color = "black";
+ }
+
+function Greeting(){
+    return (<h1 className = "heading">Hello sir,<span style = {cstyle}> {k} </span></h1>);
+}
+
+export default Greeting;
