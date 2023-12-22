2f6f78eb264cdb288495a23d224b0d6df3572f12
Shilpa <shilpa.sinha3107@gmail.com>
2023-12-22 20:26:00 +0530

    Made changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App5 from './App5';
 
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App5/>, document.getElementById('root'));
